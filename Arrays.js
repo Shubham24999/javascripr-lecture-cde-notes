deb5134a823927275a71aafdd5eb6eb08fb2fba8
SHUBHAM KUMAR GUPTA <103827393+Shubham24999@users.noreply.github.com>
2024-12-10 00:40:21 +0530

    arrayas notes

---
+++ Arrays.js
@@ -0,0 +1,20 @@
+let a1=[1,"Shubh",4,"Deep",8,10]
+let s1=a1.join(" $ ")
+
+console.log(s1);
+
+
+
+
+// a1.push("computer");
+// a1.pop();
+// a1.unshift("new element")
+// console.log(a1);
+
+// a1.shift()
+// console.log(a1);
+
+// console.log(a1[2]);
+
+// console.log(a1.length);
+
