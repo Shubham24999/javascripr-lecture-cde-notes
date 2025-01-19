7bfa34870c30296f57ea2414674fe1e62f92915b
SHUBHAM KUMAR GUPTA <103827393+Shubham24999@users.noreply.github.com>
2025-01-19 13:33:12 +0530

    variables code

--- Variable.js
+++ Variable.js
@@ -1,4 +1,5 @@
 
+var m=9;
 
 function checkVar(){
     console.log("Befor Declaration of Variable Var  "+ x);
@@ -11,23 +12,33 @@ function checkVar(){
 
     console.log("After ReDeclaration of Variable Var   "+ x);
 
+    console.log("value of i is  "+i);
+
+    console.log("m  "+m);
+    
+    
+
 }
 
 // checkVar();
 
 
 function checkLet(){
-    console.log("Befor Declaration of Variable Let"+ x);
+    
+    // console.log("Befor Declaration of Variable Let"+ x);
     
     let x;
 
     x=10;
 
-    console.log("After Declaration of Variable Let"+ x);
+    console.log("After Declaration of Variable Let "+ x);
 
-    // let x=12;
+    x=12;
 
-    // console.log("After ReDeclaration of Variable Var   "+ x);
+    console.log("After ReDeclaration of Variable Var   "+ x);
+
+    // console.log("value of j is   "+j);
+    
 
 
 }
@@ -37,13 +48,22 @@ function checkLet(){
 
 
 function checkConst(){
+
     const x=10;
-    console.log("Befor Declaration of Variable Var  "+ x);
+
+    console.log("initially of Variable const  "+ x);
     
     x=7;
-    console.log("After Declaration of Variable Var   "+ x);
+    console.log("After ReAssigning of Variable const   "+ x);
+
+     console.log("value of k is   "+k);
 
 
 }
 
 checkConst();
+
+
+var i=5;
+let j=6;
+const k=7;
--- VaribaleNames.js
+++ VaribaleNames.js
@@ -1,18 +1,47 @@
+let x="Nidhi"
+let k='8567'
+let y='Nidhi'
+let n="Deepak"
+
+let myName="deepak";
+let rName='deepak';
+let bghdsfh_gh=8;
+
+
+console.log(x);
+console.log(y);
+console.log("My friend is "+n);
+console.log(`${n} is my friend `);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 var myage=25;
 
 var mYAge=24;   
 
 // if x=10;
 
-var y=10;
+// var y=10;
 
 
 // var my Age=25;
 
 
-console.log(myage);
-console.log(mYAge);
+// console.log(myage);
+// console.log(mYAge);
 
-console.log(y);
+// console.log(y);
 
 
