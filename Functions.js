0c2be839b515494f203750b03cf73ea282222185
SHUBHAM KUMAR GUPTA <103827393+Shubham24999@users.noreply.github.com>
2025-02-27 00:08:39 +0530

    function example

---
+++ Functions.js
@@ -0,0 +1,26 @@
+
+// declaring a function return nothing 
+// function greet() {
+//     console.log("Hello, World!");
+// }
+
+
+// Calling the function
+// greet();
+
+
+// declaring a function return value using arrow function ES6
+// const subtract = (a, b) => a - b;
+
+// console.log(subtract(10, 4));
+
+
+
+
+// Function Expressions
+
+const subtract = function(x, y) {
+    return x - y;
+};
+
+console.log(subtract(10, 4));
